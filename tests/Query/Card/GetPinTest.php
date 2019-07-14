58d768ffc08522405b71db32b91e6ba72e78c00a
Gaurav Malhotra <gaurav@level5.co.uk>
2019-07-14 18:15:16 +0200

    Get Pin test added

---
+++ tests/Query/Card/GetPinTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Test\Query\Card;
+
+use Railsbank\Query\Card\GetPin;
+use Test\CommandOrQueryTest;
+
+class GetPinTest extends CommandOrQueryTest
+{
+    public function setUp()
+    {
+        $this->command = GetPin::class;
+    }
+
+    public function getCommandInputs(): array
+    {
+        return [
+            'no input throws error' => [
+                true,
+                [],
+            ],
+            'provide card id is valid' => [
+                false,
+                [ 'card_id' => '1234', ],
+            ]
+        ];
+    }
+}
