e186ee2b25f12735a12d152e9648ab9371817c95
Gaurav Malhotra <gaurav@level5.co.uk>
2019-07-17 22:08:23 +0100

    Add in Card queries

---
+++ tests/Query/Card/GetCardTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Test\Query\Card;
+
+use Railsbank\Query\Card\GetCard;
+use Test\CommandOrQueryTest;
+
+class GetCardTest extends CommandOrQueryTest
+{
+    public function setUp()
+    {
+        $this->command = GetCard::class;
+    }
+
+    public function getCommandInputs(): array
+    {
+        return [
+            'no input throws error' => [
+                true,
+                [],
+            ],
+            'provide card id is valid' => [
+                false,
+                [ 'card_id' => '1234', ],
+                [ 'card_id' => '1234', ],
+            ],
+        ];
+    }
+}
